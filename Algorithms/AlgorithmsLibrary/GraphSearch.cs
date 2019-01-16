77ce86861ac5472b3373c455b5a8f1af6a1e2f82
jags1891 <jagadeesh.bandlamudi@gmail.com>
2019-01-15 23:48:24 -0800

    DFS

---
+++ Algorithms/AlgorithmsLibrary/GraphSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmsLibrary.DataStructures;
+
+namespace AlgorithmsLibrary
+{
+    public class GraphSearch
+    {
+        public HashSet<T> DFS<T>(Graph<T> graph, T start)
+        {
+            var visited = new HashSet<T>();
+
+            if (!graph.AdjacencyList.ContainsKey(start))
+                return visited;
+
+            var stack = new Stack<T>();
+            stack.Push(start);
+
+            while (stack.Count > 0)
+            {
+                var vertex = stack.Pop();
+
+                if (visited.Contains(vertex))
+                    continue;
+
+                visited.Add(vertex);
+
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                    if (!visited.Contains(neighbor))
+                        stack.Push(neighbor);
+            }
+
+            return visited;
+        }
+    }
+}
