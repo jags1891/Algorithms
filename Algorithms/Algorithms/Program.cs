a7c1d33860601ec227bf827c9ccdf32a77115d91
jags1891 <jagadeesh.bandlamudi@gmail.com>
2019-01-15 21:02:14 -0800

    Solution

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AlgorithmsLibrary;
 
-namespace Algorithms
+namespace AlgoConsoleLib
 {
     class Program
     {
--- Algorithms/ValidParentheses/ValidParentheses.cs
+++ Algorithms/ValidParentheses/ValidParentheses.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ValidParentheses
+namespace AlgorithmsLibrary
 {
     public class ValidParentheses
     {
