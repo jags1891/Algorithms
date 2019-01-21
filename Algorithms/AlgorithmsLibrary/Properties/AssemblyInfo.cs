a4d3597d18e65e4049a9f82952cb2d325a6e3a5d
jags1891 <jagadeesh.bandlamudi@gmail.com>
2019-01-20 17:11:20 -0800

    Assembly Info

--- Algorithms/AlgorithmsLibrary/Properties/AssemblyInfo.cs
+++ Algorithms/AlgorithmsLibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ValidParentheses")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AlgorithmsLibrary")]
+[assembly: AssemblyDescription("Jagadeesh Bandlamudi")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ValidParentheses")]
+[assembly: AssemblyProduct("AlgorithmsLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
