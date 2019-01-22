e9da7496026a1deda6d2c81828f236a697bfeb44
jags1891 <jagadeesh.bandlamudi@gmail.com>
2019-01-21 20:48:28 -0800

    Word Ladder Stib

---
+++ Algorithms/AlgorithmsLibrary/DataStructures/MyLexicon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLibrary.DataStructures
+{
+    /// <summary>
+    /// English Dictionary
+    /// To to enhanced....
+    /// </summary>
+    public class MyLexicon
+    {
+        private static MyLexicon _myLexicon;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static MyLexicon Instance
+        {
+            get
+            {
+                if (_myLexicon == null)
+                    _myLexicon = new MyLexicon();
+
+                return _myLexicon;
+            }
+        }
+
+        private MyLexicon()
+        {
+
+        }
+
+        
+        static List<string> words = System.IO.File.ReadAllText(System.IO.Directory.GetCurrentDirectory()+ @"\Data\Words.txt").Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        /// <summary>
+        /// Find out if the string exist in the Lexicon.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public bool WordExists(string str)
+        {
+            return (words.Contains(str) || words.Contains(str.ToLower()) || words.Contains(str.ToUpper())) ;
+        }
+
+        /// <summary>
+        /// Returns the list of words which contains the sting that is passed
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public  List<string> WordsLike(string str)
+        {
+            return words.FindAll(s => {return s.Contains(str);});
+        }
+    }
+}
---
+++ Algorithms/AlgorithmsLibrary/Graphs/WordLadder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLibrary.Graphs
+{
+    /// <summary>
+    /// To be implemented
+    /// </summary>
+    public class WordLadder
+    {
+        /// <summary>
+        /// Given two words (beginWord and endWord), and a dictionary's word list, 
+        /// find the length of shortest transformation sequence from beginWord to endWord, such that:
+        ///     1.  Only one letter can be changed at a time.
+        ///     2.  Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+        /// 
+        /// Note:
+        ///     Return 0 if there is no such transformation sequence.
+        ///     All words have the same length.
+        ///     All words contain only lowercase alphabetic characters.
+        ///     You may assume no duplicates in the word list.
+        ///     You may assume beginWord and endWord are non-empty and are not the same.
+        ///     
+        /// </summary>
+        /// <param name="beginWord"></param>
+        /// <param name="endWord"></param>
+        /// <param name="wordList"></param>
+        /// <returns></returns>
+        public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+        {
+            return 0;
+        }
+    }
+}
