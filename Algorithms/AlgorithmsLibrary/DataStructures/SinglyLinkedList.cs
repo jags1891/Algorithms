e067d84b0c1a8a616d5ee46e92e7dcb2863a1a7d
jags1891 <jagadeesh.bandlamudi@gmail.com>
2019-01-27 12:53:14 -0800

    SinglyLinkedList

---
+++ Algorithms/AlgorithmsLibrary/DataStructures/SinglyLinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLibrary.DataStructures
+{
+    /// <summary>
+    /// SinglyLinkedListNode
+    /// </summary>
+    public class SinglyLinkedListNode
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="x"></param>
+        public SinglyLinkedListNode(int x)
+        {
+            Val = x;
+        }
+
+        /// <summary>
+        /// Value 
+        /// </summary>
+        public int Val;
+
+        /// <summary>
+        /// Pointer to the next node
+        /// </summary>
+        public SinglyLinkedListNode Next;
+
+    }
+}
