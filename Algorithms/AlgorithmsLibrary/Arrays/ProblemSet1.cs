f1a3f5aeab6e35590c745377150483087003fe11
jags1891 <jagadeesh.bandlamudi@gmail.com>
2019-02-19 00:48:50 -0800

    Added some Array and string problems

---
+++ Algorithms/AlgorithmsLibrary/Arrays/ProblemSet1.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsLibrary.Arrays
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProblemSet1
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public int GetMostFreqentElement(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                return -1;
+            else if (arr.Length == 1)
+                return arr[0];
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int maxKey = 0;
+            int maxVal = -1;
+   
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (dic.ContainsKey(arr[i]))
+                    dic[arr[i]]++;
+                else
+                    dic[arr[i]] = 1;
+
+                if(dic[arr[i]]> maxVal)
+                {
+                    maxKey = i;
+                    maxVal = dic[arr[i]];
+                }
+            }
+
+            return maxKey;
+        }
+
+        /// <summary>
+        /// Get common elements in 2 sorted arrays
+        /// Both the input arrays are sorted
+        /// </summary>
+        /// <param name="arr1"></param>
+        /// <param name="arr2"></param>
+        /// <returns></returns>
+        public int[] GetCommonElements(int[] arr1, int[] arr2)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            int arr1Ind = 0;
+            int arr2Ind = 0;
+
+            while(arr1Ind<arr1.Length && arr2Ind < arr2.Length)
+            {
+                if (arr1[arr1Ind] == arr2[arr2Ind])
+                {
+                    arrayList.Add(arr1[arr1Ind++]);
+                    arr2Ind++;
+                }
+                else if (arr1[arr1Ind] > arr2[arr2Ind])
+                    arr2Ind++;
+                else
+                    arr1Ind++;
+            }
+
+            return arrayList.OfType<int>().ToArray();
+
+        }
+
+        /// <summary>
+        /// Rerurn true if one input array is the rotation of other.
+        /// No duplicates in the arrays
+        /// </summary>
+        /// <param name="arr1"></param>
+        /// <param name="arr2"></param>
+        /// <returns></returns>
+        public bool IsRotation(int[] arr1, int[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+                return false;
+
+            int key = arr1[0];
+            int startInd = -1;
+
+            for(int i=0; i < arr2.Length; i++)
+            {
+                if (arr2[i] == key)
+                {
+                    startInd = i;
+                    break;
+                } 
+            }
+
+            if (startInd == -1)
+                return false;
+
+            for(int i=0; i < arr1.Length; i++)
+            {
+                int j = (i + startInd) % arr1.Length;
+                if (arr1[i] != arr2[j])
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ Algorithms/AlgorithmsLibrary/Strings/ProblemSett1.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorithmsLibrary.Strings
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProblemSett1
+    {
+        /// <summary>
+        /// Retrun 1st non repeating char of the string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public char GetNonRepeatingChar(string str)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            char res = ' ';
+
+            foreach (char c in str)
+            {
+                if (dic.ContainsKey(c))
+                    dic[c]++;
+                else
+                    dic[c] = 1;
+
+                if (dic[c] == 1)
+                    res = c;
+            }
+
+            // To return First non repeated char
+            foreach (char c in str)
+            {
+                if (dic[c] == 1)
+                    return c;
+            }
+
+            return res;
+        }
+
+        static int NO_OF_CHAR = 256; //8 bit extended ASCHII
+
+        /// <summary>
+        /// using array
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public char GetNonRepeatingChar2(string str)
+        {
+            int[] temp = new int[NO_OF_CHAR];
+
+            foreach (char c in str)
+                temp[c]++;
+
+            // To return First non repeated char
+            foreach (char c in str)
+                if (temp[c] == 1)
+                    return c;
+
+            return ' ';
+        }
+
+        /// <summary>
+        /// Check if edit distance between two strings is one
+        /// An edit between two strings is one of the following changes.
+        ///     Add a character
+        ///     Delete a character
+        ///     Change a character
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public bool IsOneAway(string s1, string s2)
+        {
+            if (s1.Length - s2.Length > 1 || s2.Length - s1.Length > 1)
+                return false;
+  
+            int diffCount = 0;
+            int s1Ind = 0;
+            int s2Ind = 0;
+
+            while(s1Ind < s1.Length && s2Ind < s2.Length)
+            {
+                if(s1[s1Ind]==s2[s2Ind])
+                {
+                    s1Ind++;
+                    s2Ind++;
+                }
+                else if(s1.Length> s2.Length)
+                {
+                    diffCount++;
+                    s1Ind++;
+                }
+                else if(s1.Length < s2.Length)
+                {
+                    diffCount++;
+                    s2Ind++;
+                }
+                else
+                {
+                    diffCount++;
+                    s2Ind++;
+                    s1Ind++;
+                }
+
+                if (diffCount > 1)
+                    return false;
+            }
+
+            return true;
+        }
+
+              
+    }
+
+}
