46c12e0d81f2daf2db5ddf256fc2df000cb5e2bb
jags1891 <jagadeesh.bandlamudi@gmail.com>
2019-02-23 12:41:11 -0800

    ArrayIndexEquality

---
+++ Algorithms/AlgorithmsLibrary/Arrays/ArrayIndexEquality.cs
@@ -0,0 +1,41 @@
+﻿namespace AlgorithmsLibrary.Arrays
+{
+    /// <summary>
+    /// Array Index & Element Equality
+    /// </summary>
+    public class ArrayIndexEquality
+    {
+        /// <summary>
+        /// Given a sorted array arr of distinct integers, 
+        /// write a function indexEqualsValueSearch that returns the lowest index i for which arr[i] == i. 
+        /// Return -1 if there is no such index. 
+        /// Analyze the time and space complexities of your solution and explain its correctness.
+        /// 
+        /// Examples:
+        /// input: arr = [-8,0,2,5]
+        /// output: 2 # since arr[2] == 2
+        /// input: arr = [-1,0,3,6]
+        /// output: -1 # since no index in arr satisfies arr[i] == i.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int IndexEqualsValueSearch(int[] arr)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (arr[mid] - mid < 0)
+                    start = mid + 1;
+                else if (arr[mid] - mid == 0 && (arr[mid] == 0 || arr[mid - 1] - (mid - 1) < 0))
+                    return mid;
+                else
+                    end = mid - 1;
+            }
+
+            return -1;
+        }
+    }
+}
